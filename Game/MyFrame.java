5810606707db507ed5fd8551c53c94b12a44a90c
Andrew Shashin 2017 <you@example.com>
2015-05-14 08:38:21 -0500

    barriers

--- Game/MyFrame.java
+++ Game/MyFrame.java
@@ -18,8 +18,20 @@ import java.awt.geom.*;
 	  
         public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        Line2D lin = new Line2D.Float(100, 100, 250, 260);
         
-        g2.draw(lin);
+        Line2D lin1= new Line2D.Float(160,0,160,280);
+        Line2D lin2= new Line2D.Float(0,480,280,480);
+        Line2D lin3= new Line2D.Float(280,480,400,280);
+        Line2D lin4= new Line2D.Float(400,280,560,440);
+        Line2D lin5= new Line2D.Float(440,0,560,200);
+        Line2D lin6= new Line2D.Float(640,400,800,240);
+        Line2D lin7= new Line2D.Float(1000,240,1000,480);
+        g2.draw(lin1);
+        g2.draw(lin2);
+        g2.draw(lin3);
+        g2.draw(lin4);
+        g2.draw(lin5);
+        g2.draw(lin6);
+        g2.draw(lin7);
      }
 	}
